f4b581a7fbf72f817bc519b0891053847a6ff74d
Teagan King <tking@ucar.edu>
2022-03-25 15:03:18 -0600

    general code cleanup

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,5 +1,4 @@
 """
-
 Authors
 -------
     - Steve Yeager
@@ -12,22 +11,20 @@ Use
 
 Dependencies
 ------------
-    The user must have an activated conda environment which includes xarray, numpy, glob, functools, and sys.
+    The user must have an activated conda environment which includes xarray, numpy, glob, and functools.
 """
 
 import xarray as xr
 import numpy as np
 import glob
 from functools import partial
-import sys
 
 
-
-def file_dict(filetempl,filetype,mem,stmon):
-    """ returns a dictionary of filepaths keyed by initialization year, 
+def file_dict(filetempl, filetype, mem, stmon):
+    """ Returns a dictionary of filepaths keyed by initialization year, 
     for a given experiment, field, ensemble member, and initialization month
 
-    parameters
+    Parameters
     ----------
     filetempl : str
         file template
@@ -38,35 +35,34 @@ def file_dict(filetempl,filetype,mem,stmon):
     stmon : str
         month
 
-    returns
-    -------    
+    Returns
+    -------
     filepaths : dict
         dictionary containing filepaths keyed by initialization year
     """
 
-
     memstr = '{0:03d}'.format(mem)
     monstr = '{0:02d}'.format(stmon)
     filepaths = {}
-    
-    filetemp = filetempl.replace('MM',monstr).replace('EEE',memstr)
 
-    #find all the relevant files
+    filetemp = filetempl.replace('MM', monstr).replace('EEE', memstr)
+
+    # find all the relevant files
     files = sorted(glob.glob(filetemp))
-        
+
     for file in files:
-        #isolate initialization year from the file name
+        # isolate initialization year from the file name
         ystr = file.split(filetype)[0]
         y0 = int(ystr[-11:-7])
-        filepaths[y0]=file
-        
+        filepaths[y0] = file
+
     return filepaths
 
 
-def nested_file_list_by_year(filetemplate,filetype,ens,field,firstyear,lastyear,stmon):
+def nested_file_list_by_year(filetemplate, filetype, ens, firstyear, lastyear, stmon):
     """ retrieves a nested list of files for these start years and ensemble members
 
-    parameters
+    Parameters
     ----------
     filetemplate : str
         file template
@@ -74,8 +70,6 @@ def nested_file_list_by_year(filetemplate,filetype,ens,field,firstyear,lastyear,
         file ending
     ens : int (?)
         ensemble member
-    field : (?)
-        (not used?)
     firstyear : int
         first start year
     lastyear : int
@@ -83,42 +77,42 @@ def nested_file_list_by_year(filetemplate,filetype,ens,field,firstyear,lastyear,
     stmon : str
         month
 
-    returns
-    ------- 
+    Returns
+    -------
     files, yrs[ix==1]: list (?)
         nested list of files
     """
 
-    ens = np.array(range(ens))+1
-    yrs = np.arange(firstyear,lastyear+1)
+    ens = np.array(range(ens)) + 1
+    yrs = np.arange(firstyear, lastyear + 1)
     files = []    # a list of lists, dim0=start_year, dim1=ens
-    ix = np.zeros(yrs.shape)+1
-    
-    for yy,i in zip(yrs,range(len(yrs))):
+    ix = np.zeros(yrs.shape) + 1
+
+    for yy, i in zip(yrs, range(len(yrs))):
         ffs = []  # a list of files for this yy
         file0 = ''
-        first = True
         for ee in ens:
-            filepaths = file_dict(filetemplate,filetype,ee,stmon)
-            #append file if it is new
+            filepaths = file_dict(filetemplate, filetype, ee, stmon)
+            # append file if it is new
             if yy in filepaths.keys():
                 file = filepaths[yy]
                 if file != file0:
                     ffs.append(file)
                     file0 = file
-        
-        #append this ensemble member to files
-        if ffs:  #only append if you found files
+
+        # append this ensemble member to files
+        if ffs:  # only append if you found files
             files.append(ffs)
         else:
             ix[i] = 0
-    return files,yrs[ix==1]
+    return files, yrs[ix == 1]
 
 
-def get_monthly_data(filetemplate,filetype,ens,nlead,field,firstyear,lastyear,stmon,preproc,chunks={}):
-    """ returns a dask array containing the requested hindcast ensemble
+def get_monthly_data(filetemplate, filetype, ens, nlead, field, firstyear, lastyear, stmon, preproc, chunks={}):
+    """
+    Returns a dask array containing the requested hindcast ensemble
 
-    parameters
+    Parameters
     ----------
     nfiletemplate : str
         file template
@@ -128,7 +122,7 @@ def get_monthly_data(filetemplate,filetype,ens,nlead,field,firstyear,lastyear,st
         ensemble member
     lead : (?)
         (?)
-    field : (?) 
+    field : (?)
         (?)
     firstyear : int
         first start year
@@ -141,31 +135,30 @@ def get_monthly_data(filetemplate,filetype,ens,nlead,field,firstyear,lastyear,st
     chunks : (array ?)
         chunks for dask array
 
-    returns
-    ------- 
+    Returns
+    -------
     ds0 : dask array
         dask array containing requested hindcast ensemble
     """
 
-    file_list,yrs = nested_file_list_by_year(filetemplate,filetype,ens,field,firstyear,lastyear,stmon)
+    file_list, yrs = nested_file_list_by_year(filetemplate, filetype, ens, firstyear, lastyear, stmon)
 
-    ds0 = xr.open_mfdataset(
-    file_list,
-    combine="nested",
-    # concat_dim depends on how file_list is ordered; 
-    # inner most list of datasets is combined along "M"; 
-    # then the outer list is combined along "Y"
-    concat_dim=["Y","M"],
-    parallel=True,
-    data_vars=[field],
-    coords="minimal",
-    compat="override",
-    preprocess=partial(preproc,nlead=nlead,field=field),chunks=chunks)
-    
+    ds0 = xr.open_mfdataset(file_list,
+                            combine="nested",
+                            # concat_dim depends on how file_list is ordered;
+                            # inner most list of datasets is combined along "M"; ]
+                            # then the outer list is combined along "Y"
+                            concat_dim=["Y", "M"],
+                            parallel=True,
+                            data_vars=[field],
+                            coords="minimal",
+                            compat="override",
+                            preprocess=partial(preproc, nlead=nlead, field=field), chunks=chunks)
+
     # assign final attributes
     ds0["Y"] = yrs
     ds0["M"] = np.arange(ds0.sizes["M"]) + 1
-    
+
     # reorder into desired format (Y,L,M,...)
-    ds0 = ds0.transpose("Y","L","M",...)
+    ds0 = ds0.transpose("Y", "L", "M", ...)
     return ds0
--- utils/stat_utils.py
+++ utils/stat_utils.py
@@ -21,15 +21,17 @@ import sys
 import cftime
 import xskillscore as xs
 
+
 def cor_ci_bootyears(ts1, ts2, seed=None, nboots=1000, conf=95):
     """
-    parameters
+    Parameters
     ----------
-    returns
+    Returns
     -------
     """
-    ptilemin = (100.-conf)/2.
-    ptilemax = conf + (100-conf)/2.
+
+    ptilemin = (100. - conf) / 2.
+    ptilemax = conf + (100 - conf) / 2.
 
     if (ts1.size != ts2.size):
         print("The two arrays must have the same size")
@@ -39,38 +41,39 @@ def cor_ci_bootyears(ts1, ts2, seed=None, nboots=1000, conf=95):
         np.random.seed(seed)
 
     samplesize = ts1.size
-    ranu = np.random.uniform(0, samplesize, nboots*samplesize)
+    ranu = np.random.uniform(0, samplesize, nboots * samplesize)
     ranu = np.floor(ranu).astype(int)
 
     bootdat1 = np.array(ts1[ranu])
     bootdat2 = np.array(ts2[ranu])
     bootdat1 = bootdat1.reshape([samplesize, nboots])
     bootdat2 = bootdat2.reshape([samplesize, nboots])
-   
- 
+
     bootcor = xr.corr(xr.DataArray(bootdat1), xr.DataArray(bootdat2), dim='dim_0')
-    minci = np.percentile(bootcor,ptilemin)
-    maxci = np.percentile(bootcor,ptilemax)
+    minci = np.percentile(bootcor, ptilemin)
+    maxci = np.percentile(bootcor, ptilemax)
 
     return minci, maxci
+
 
 def detrend_linear(dat, dim):
     """
     linear detrend dat along the axis dim
-    parameters
+    Parameters
     ----------
-    returns
+    Returns
     -------
     """
     params = dat.polyfit(dim=dim, deg=1)
     fit = xr.polyval(dat[dim], params.polyfit_coefficients)
-    dat = dat-fit
+    dat = dat - fit
     return dat
+
 
 def remove_drift(da, da_time, y1, y2):
     """
     Function to convert raw DP DataArray into anomaly DP DataArray with leadtime-dependent climatology removed.
-    parameters
+    Parameters
     ----------
     da : DP DataArray
         Raw DP DataArray with dimensions (Y,L,M,...)
@@ -80,83 +83,90 @@ def remove_drift(da, da_time, y1, y2):
         Start year of climatology
     y2 : int
         End year of climatology
-    
-    returns
-    -------    
+
+    Returns
+    -------
     da_anom : DP DataArray
         De-drifted DP DataArray
     da_climo : DP DataArray
         Leadtime-dependent climatology
-    
+
     Author: E. Maroon (modified by S. Yeager)
     """
 
-    d1 = cftime.DatetimeNoLeap(y1,1,1,0,0,0)
-    d2 = cftime.DatetimeNoLeap(y2,12,31,23,59,59)
-    masked_period = da.where((da_time>d1) & (da_time<d2))
+    d1 = cftime.DatetimeNoLeap(y1, 1, 1, 0, 0, 0)
+    d2 = cftime.DatetimeNoLeap(y2, 12, 31, 23, 59, 59)
+    masked_period = da.where((da_time > d1) & (da_time < d2))
     da_climo = masked_period.mean('M').mean('Y')
     da_anom = da - da_climo
     return da_anom, da_climo
 
-def leadtime_skill_seas(mod_da,mod_time,obs_da,detrend=False):
-    """ 
-    Computes a suite of deterministic skill metrics given two DataArrays corresponding to model and observations, which 
+
+def leadtime_skill_seas(mod_da, mod_time, obs_da, detrend=False):
+    """
+    Computes a suite of deterministic skill metrics given two DataArrays corresponding to model and observations, which
     must share the same lat/lon coordinates (if any). Assumes time coordinates are compatible
     (can be aligned). Both DataArrays should represent 3-month seasonal averages (DJF, MAM, JJA, SON).
-    
-    parameters
+
+    Parameters
     ----------
     mod_da: DataArray
         a seasonally-averaged hindcast DataArray dimensioned (Y,L,M,...)
     mod_time: DataArray
         a hindcast time DataArray dimensioned (Y,L). NOTE: assumes mod_time.dt.month
-    returns
+    Returns
     -------
     xr_dataset : DataArray
         the mid-month of a 3-month seasonal average (e.g., mon=1 ==> "DJF").
     obs_da: DataArray (not returned?)
         an OBS DataArray dimensioned (season,year,...)
     """
-    seasons = {1:'DJF',4:'MAM',7:'JJA',10:'SON'}
-    corr_list = []; pval_list = []; rmse_list = []; msss_list = []; rpc_list = []; pers_list = []
+    seasons = {1: 'DJF', 4: 'MAM', 7: 'JJA', 10: 'SON'}
+    corr_list = []
+    pval_list = []
+    rmse_list = []
+    msss_list = []
+    rpc_list = []
+    pers_list = []
     # convert L to leadtime values:
     leadtime = mod_da.L - 2
     for i in mod_da.L.values:
-        ens_ts = mod_da.sel(L=i).rename({'Y':'time'})
+        ens_ts = mod_da.sel(L=i).rename({'Y': 'time'})
         ens_time_year = mod_time.sel(L=i).dt.year.data
         ens_time_month = mod_time.sel(L=i).dt.month.data[0]
-        obs_ts = obs_da.sel(season=seasons[ens_time_month]).rename({'year':'time'})
-        ens_ts = ens_ts.assign_coords(time=("time",ens_time_year))
-        a,b = xr.align(ens_ts,obs_ts)
+        obs_ts = obs_da.sel(season=seasons[ens_time_month]).rename({'year': 'time'})
+        ens_ts = ens_ts.assign_coords(time=("time", ens_time_year))
+        a, b = xr.align(ens_ts, obs_ts)
         if detrend:
-            a = detrend_linear(a,'time')
-            b = detrend_linear(b,'time')
+            a = detrend_linear(a, 'time')
+            b = detrend_linear(b, 'time')
         amean = a.mean('M')
         sigobs = b.std('time')
         sigsig = amean.std('time')
         sigtot = a.std('time').mean('M')
-        r = xs.pearson_r(amean,b,dim='time')
-        rpc = r/(sigsig/sigtot)
+        r = xs.pearson_r(amean, b, dim='time')
+        rpc = r / (sigsig / sigtot)
         corr_list.append(r)
-        rpc_list.append(rpc.where(r>0))
-        rmse_list.append(xs.rmse(amean,b,dim='time')/sigobs)
-        msss_list.append(1-(xs.mse(amean,b,dim='time')/b.var('time')))
-        pval_list.append(xs.pearson_r_eff_p_value(amean,b,dim='time'))
-    corr = xr.concat(corr_list,leadtime)
-    pval = xr.concat(pval_list,leadtime)
-    rmse = xr.concat(rmse_list,leadtime)
-    msss = xr.concat(msss_list,leadtime)
-    rpc = xr.concat(rpc_list,leadtime)
-    xr_dataset = xr.Dataset({'corr':corr,'pval':pval,'nrmse':rmse,'msss':msss,'rpc':rpc})
+        rpc_list.append(rpc.where(r > 0))
+        rmse_list.append(xs.rmse(amean, b, dim='time') / sigobs)
+        msss_list.append(1 - (xs.mse(amean, b, dim='time') / b.var('time')))
+        pval_list.append(xs.pearson_r_eff_p_value(amean, b, dim='time'))
+    corr = xr.concat(corr_list, leadtime)
+    pval = xr.concat(pval_list, leadtime)
+    rmse = xr.concat(rmse_list, leadtime)
+    msss = xr.concat(msss_list, leadtime)
+    rpc = xr.concat(rpc_list, leadtime)
+    xr_dataset = xr.Dataset({'corr': corr, 'pval': pval, 'nrmse': rmse, 'msss': msss, 'rpc': rpc})
     return xr_dataset
 
-def leadtime_skill_seas_resamp(mod_da,mod_time,obs_da,sampsize,N,detrend=False):
-    """ 
+
+def leadtime_skill_seas_resamp(mod_da, mod_time, obs_da, sampsize, N, detrend=False):
+    """
     Same as leadtime_skill_seas(), but this version resamples the mod_da member dimension (M) to generate
-    a distribution of skill scores using a smaller ensemble size (N, where N<M). Returns the 
+    a distribution of skill scores using a smaller ensemble size (N, where N<M). Returns the
     mean of the resampled skill score distribution.
-    
-    parameters
+
+    Parameters
     ----------
     mod_da: DataArray
         a seasonally-averaged hindcast DataArray dimensioned (Y,L,M,...)
@@ -171,48 +181,52 @@ def leadtime_skill_seas_resamp(mod_da,mod_time,obs_da,sampsize,N,detrend=False):
     detrend : bool
         (?)
 
-    returns
+    Returns
     -------
     dsout : int (?)
         mean of resampled skill score distribution
     """
     dslist = []
-    seasons = {1:'DJF',4:'MAM',7:'JJA',10:'SON'}
+    seasons = {1: 'DJF', 4: 'MAM', 7: 'JJA', 10: 'SON'}
     # convert L to leadtime values:
     leadtime = mod_da.L - 2
     # Perform resampling
-    if (not N<mod_da.M.size):
+    if (not N < mod_da.M.size):
         raise ValueError('ERROR: expecting resampled ensemble size to be less than original')
     mod_da_r = xs.resample_iterations(mod_da.chunk(), sampsize, 'M', dim_max=N)
     for l in mod_da_r.iteration.values:
-        corr_list = []; pval_list = []; rmse_list = []; msss_list = []; rpc_list = []; pers_list = []
+        corr_list = []
+        pval_list = []
+        rmse_list = []
+        msss_list = []
+        rpc_list = []
+        pers_list = []
         for i in mod_da.L.values:
-            ens_ts = mod_da_r.sel(iteration=l).sel(L=i).rename({'Y':'time'})
+            ens_ts = mod_da_r.sel(iteration=l).sel(L=i).rename({'Y': 'time'})
             ens_time_year = mod_time.sel(L=i).dt.year.data
             ens_time_month = mod_time.sel(L=i).dt.month.data[0]
-            obs_ts = obs_da.sel(season=seasons[ens_time_month]).rename({'year':'time'})
-            ens_ts = ens_ts.assign_coords(time=("time",ens_time_year))
-            a,b = xr.align(ens_ts,obs_ts)
+            obs_ts = obs_da.sel(season=seasons[ens_time_month]).rename({'year': 'time'})
+            ens_ts = ens_ts.assign_coords(time=("time", ens_time_year))
+            a, b = xr.align(ens_ts, obs_ts)
             if detrend:
-                a = detrend_linear(a,'time')
-                b = detrend_linear(b,'time')
+                a = detrend_linear(a, 'time')
+                b = detrend_linear(b, 'time')
             amean = a.mean('M')
             sigobs = b.std('time')
             sigsig = amean.std('time')
             sigtot = a.std('time').mean('M')
-            r = xs.pearson_r(amean,b,dim='time')
-            rpc = r/(sigsig/sigtot)
+            r = xs.pearson_r(amean, b, dim='time')
+            rpc = r / (sigsig / sigtot)
             corr_list.append(r)
-            rpc_list.append(rpc.where(r>0))
-            rmse_list.append(xs.rmse(amean,b,dim='time')/sigobs)
-            msss_list.append(1-(xs.mse(amean,b,dim='time')/b.var('time')))
-            pval_list.append(xs.pearson_r_eff_p_value(amean,b,dim='time'))
-        corr = xr.concat(corr_list,leadtime)
-        pval = xr.concat(pval_list,leadtime)
-        rmse = xr.concat(rmse_list,leadtime)
-        msss = xr.concat(msss_list,leadtime)
-        rpc = xr.concat(rpc_list,leadtime)
-        dslist.append(xr.Dataset({'corr':corr,'pval':pval,'rmse':rmse,'msss':msss,'rpc':rpc}))
-    dsout = xr.concat(dslist,dim='iteration').mean('iteration').compute()
+            rpc_list.append(rpc.where(r > 0))
+            rmse_list.append(xs.rmse(amean, b, dim='time') / sigobs)
+            msss_list.append(1 - (xs.mse(amean, b, dim='time') / b.var('time')))
+            pval_list.append(xs.pearson_r_eff_p_value(amean, b, dim='time'))
+        corr = xr.concat(corr_list, leadtime)
+        pval = xr.concat(pval_list, leadtime)
+        rmse = xr.concat(rmse_list, leadtime)
+        msss = xr.concat(msss_list, leadtime)
+        rpc = xr.concat(rpc_list, leadtime)
+        dslist.append(xr.Dataset({'corr': corr, 'pval': pval, 'rmse': rmse, 'msss': msss, 'rpc': rpc}))
+    dsout = xr.concat(dslist, dim='iteration').mean('iteration').compute()
     return dsout
-
