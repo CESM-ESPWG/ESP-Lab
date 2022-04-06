7d73c2b1c9834b68ce2e75c32c0e1a7e5a74bc38
Teagan King <98482480+TeaganKing@users.noreply.github.com>
2022-04-06 12:40:12 -0600

    Update data_access.py

--- esp_lab/data_access.py
+++ esp_lab/data_access.py
@@ -3,7 +3,9 @@ This module provides utilities which support input/output processes.
 Functions in this module can provide methods to return dictionaries
 of filepaths keyed by initialization year, nested lists of files
 for particular start years and ensemble members, and dask arrays
-containing particular hindcast ensembles.
+containing particular hindcast ensembles. This module also provides
+preprocessing which can assist in using intake-esm in conjunction
+with that stats-utils and io_utils packages.
 
 Authors
 -------
@@ -21,9 +23,10 @@ Dependencies
     xarray, numpy, glob, and functools.
 """
 
-import xarray as xr
-import numpy as np
+import cftime
 import glob
+import numpy as np
+import xarray as xr
 from functools import partial
 
 
@@ -173,3 +176,59 @@ def get_monthly_data(filetemplate, filetype, ens, nlead, field,
     # reorder into desired format (Y,L,M,...)
     ds0 = ds0.transpose("Y", "L", "M", ...)
     return ds0
+
+
+def time_set_midmonth(ds, time_name):
+    """
+    Return copy of ds with values of ds[time_name] replaced with mid-month
+    values (day=15) rather than end-month values.
+    Parameters
+    ----------
+    ds : xarray
+        xarray dataset which currently has end month values
+        that will be replaced with mid month values
+    time_name : str
+        name of time component, eg 'time'
+    Returns
+    -------
+    ds : xarray
+        xarray dataset with end month values replaced with mid month values
+    """
+    year = ds[time_name].dt.year
+    month = ds[time_name].dt.month
+    year = xr.where(month == 1, year-1, year)
+    month = xr.where(month == 1, 12, month-1)
+    nmonths = len(month)
+    newtime = [cftime.DatetimeNoLeap(year[i], month[i], 15) for i in range(nmonths)]
+    ds[time_name] = newtime
+
+    return ds
+
+
+def preprocessor(ds0, nlead, field):
+    """ This preprocessor is applied on an individual timeseries file basis.
+    It will return a monthly mean CAM field with centered time coordinate.
+    Edit this appropriately for your analysis to speed up processing.
+    Parameters
+    ----------
+    ds0 : xarray
+        timeseries xarray dataset that requires preprocessing
+    nlead : int
+        (?)
+    field : str
+        variable to be examined, eg 'TREFHT'
+    Returns
+    -------
+    d0 : xarray
+        xarray dataset of monthly mean CAM field with centered time coordinate
+    """
+    d0 = time_set_midmonth(ds0, 'time')
+    d0 = d0.isel(time=slice(0, nlead))
+    d0 = d0.assign_coords({"lon": ds0.lon, "lat": ds0.lat})
+    d0 = d0.assign_coords(L=("time", np.arange(d0.sizes["time"])+1))
+    d0 = d0.swap_dims({"time": "L"})
+    d0 = d0.reset_coords(["time"])
+    d0["time"] = d0.time.expand_dims("Y")
+    d0 = d0[[field, 'time']]
+    d0 = d0.chunk({'L': -1})
+    return d0
