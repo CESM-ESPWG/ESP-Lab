9e09999e141114296ff03378ec4904161181da05
Teagan <tking@ucar.edu>
2022-04-27 17:02:50 -0600

    rtd22

--- esp_lab/__init__.py
+++ esp_lab/__init__.py
@@ -1,6 +1,7 @@
 from pkg_resources import DistributionNotFound, get_distribution
 from .data_access import get_monthly_data
 from .data_access import preprocessor
+from .stats import leadtime_skill_seas_resamp
 
 try:
     __version__ = get_distribution(__name__).version
