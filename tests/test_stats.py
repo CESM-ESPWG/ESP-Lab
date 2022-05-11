a081f75aac38a76f0f39f8483a296fe021e6eae8
Teagan King <98482480+TeaganKing@users.noreply.github.com>
2022-05-11 13:56:28 -0600

    Create test_stats.py

---
+++ tests/test_stats.py
@@ -0,0 +1,50 @@
+import sys
+
+import pytest
+
+from esp_lab.stats import cor_ci_bootyears, detrend_linear, leadtime_skill_seas, leadtime_skill_seas_resamp, remove_drift
+
+
+def test_cor_ci_bootyears():
+    """
+    Test the cor_ci_bootyears function.
+    """
+    test = True
+
+    assert test
+
+
+def test_detrend_linear():
+    """
+    Test the detrend_linear function.
+    """
+    test = True
+
+    assert test
+
+
+def test_leadtime_skill_seas():
+    """
+    Test the leadtime_skill_seas function.
+    """
+    test = True
+
+    assert test
+    
+
+def test_leadtime_skill_seas_resamp():
+    """
+    Test the leadtime_skill_seas_resamp function.
+    """
+    test = True
+
+    assert test
+
+
+def test_remove_drift():
+    """
+    Test the remove_drift function.
+    """
+    test = True
+
+    assert test
