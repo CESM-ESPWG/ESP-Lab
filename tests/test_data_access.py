23503511ba26816776f339a6d0c914dc77327c28
Teagan King <98482480+TeaganKing@users.noreply.github.com>
2022-05-11 13:53:57 -0600

    Create test_data_access.py

---
+++ tests/test_data_access.py
@@ -0,0 +1,50 @@
+import sys
+
+import pytest
+
+from esp_lab.data_access import time_set_midmonth, file_dict, get_monthly_data, nested_file_list_by_year, preprocessor
+
+
+def test_time_set_midmonth():
+    """
+    Test the time_set_midmonth function.
+    """
+    test = True
+
+    assert test
+
+
+def test_file_dict():
+    """
+    Test the file_dict function.
+    """
+    test = True
+
+    assert test
+
+
+def test_get_monthly_data():
+    """
+    Test the get_monthly_data function.
+    """
+    test = True
+
+    assert test
+    
+
+def test_nested_file_list_by_year():
+    """
+    Test the nested_file_list_by_year function.
+    """
+    test = True
+
+    assert test
+
+
+def test_preprocessor():
+    """
+    Test the preprocessor function.
+    """
+    test = True
+
+    assert test
