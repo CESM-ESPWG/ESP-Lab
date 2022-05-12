acd38742e733c186abc0e83132ad2508bc4d5e23
Teagan King <98482480+TeaganKing@users.noreply.github.com>
2022-05-12 11:38:16 -0600

    Update test_stats.py

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -14,28 +14,50 @@ def test_cor_ci_bootyears():
     """
     Test the cor_ci_bootyears function.
     """
-    ts1 = [1, 2, 3, 1, 2, 3, 1, 2, 3]
-    ts2 = [4, 5, 6, 7, 8, 9, 1, 2, 3]
-    
-    result = cor_ci_bootyears(ts1, ts2)
-    
-    print("result {}".format(result))
-    
-    # assert result[0] == minci
-    # assert result[1] == maxci
-    
-    test = True
+    ts1 = np.array([1, 2, 3, 1, 2, 3, 1, 2, 3])
+    ts2 = np.array([4, 5, 6, 7, 8, 9, 1, 2, 3])
 
-    assert test
+    result12 = cor_ci_bootyears(ts1, ts2)
+    result11 = cor_ci_bootyears(ts1, ts1)
+
+    assert result12[0] < -0.4  # -0.43306723784214496
+    assert result12[1] > 0.8  # 0.8097240500333097
+
+    # An array should be fully correlated with itself
+    assert result11[0] > 0.999
+    assert result11[1] < 1.001
 
 
 def test_detrend_linear():
     """
     Test the detrend_linear function.
     """
-    test = True
 
-    assert test
+    # Create some example data.
+    tsize = 10
+    xsize = 2
+    ysize = 2
+
+    # create array of nan's with correct size
+    data = np.ones((tsize, xsize, ysize)) * np.nan
+
+    # fill array with data
+    data[:, 0, 0] = np.linspace(0, 10, tsize)
+    data[:, 1, 0] = np.logspace(0, 1, tsize)
+    data[:, 0, 1] = np.logspace(1.5, 0.5, tsize)
+    data[:, 1, 1] = np.linspace(40, 10, tsize)
+
+    # Put the data in a xarray.Dataset.
+    ds = xr.Dataset({"data": (["time", "x", "y"], data)})
+
+    # Apply polyfit.
+    fit = ds.polyfit(dim="time", deg=1)
+    final_dat = ds - fit
+
+    assert final_dat.dims == {'x': 2, 'y': 2, 'degree': 2}
+    assert (final_dat['x'].data == np.array([0, 1])).all()
+    assert (final_dat['y'].data == np.array([0, 1])).all()
+    assert (final_dat['degree'].data == np.array([1, 0])).all()
 
 
 def test_leadtime_skill_seas():
