443a9f9ea2282b897e3aad3ede49ee70ed0f5301
Teagan King <98482480+TeaganKing@users.noreply.github.com>
2022-05-12 09:51:26 -0600

    Update test_stats.py

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -14,6 +14,16 @@ def test_cor_ci_bootyears():
     """
     Test the cor_ci_bootyears function.
     """
+    ts1 = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+    ts2 = [4, 5, 6, 7, 8, 9, 1, 2, 3]
+    
+    result = cor_ci_bootyears(ts1, ts2)
+    
+    print("result {}".format(result))
+    
+    # assert result[0] == minci
+    # assert result[1] == maxci
+    
     test = True
 
     assert test
