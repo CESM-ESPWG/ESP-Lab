769743965f8f95571a809d4255ba9dd664553899
Stephen Yeager <yeager@ucar.edu>
2022-07-21 15:41:04 -0600

    new functions added in stats.py

--- esp_lab/stats.py
+++ esp_lab/stats.py
@@ -316,10 +316,256 @@ def remove_drift(da, da_time, y1, y2):
 
     # mask data array outside of selected time
     masked_period = da.where((da_time > d1) & (da_time < d2))
-    da_climo = masked_period.mean('M').mean('Y')
+    
+    # compute lead-time dependent climatology
+    if ('M' in masked_period.dims):
+        da_climo = masked_period.mean('M').mean('Y')
+    else:
+        da_climo = masked_period.mean('Y')
 
     # De-drifted DP data array is data array with
     # leadtime-dependent climotology subtracted
     da_anom = da - da_climo
 
     return da_anom, da_climo
+
+def compute_skill_annual(mod_da,mod_time,obs_da,nleadavg=1,nleads=1,resamp=0,detrend=False):
+    """
+    Computes a suite of skill scores for annual data. Option to use xskillscore resampling to
+    compute the mean variance of individual member time series ("sigma_total").
+    Assumes mod_time and obs_da.time both contain integer year values.
+    """
+    corr_list = []; pval_list = []; rmse_list = []; msss_list = []; rpc_list = []
+    sigobs_list = []; sigsig_list = []; sigtot_list = []; s2t_list = []
+    
+    lvals = np.arange(nleadavg)
+    lvalsda = xr.DataArray(np.arange(nleads)+1,dims="L",name="L")
+    for i in range(nleads):
+        leadisel = lvals + i 
+        ens_ts = mod_da.isel(L=leadisel).mean('L').rename({'Y':'time'})
+        ens_time_year = mod_time.isel(L=leadisel).mean('L')
+        ens_ts = ens_ts.assign_coords(time=("time",ens_time_year.data))
+        a,b = xr.align(ens_ts,obs_ts)
+        b = b - b.mean('time')
+        if detrend:
+                a = detrend_linear(a,'time')
+                b = detrend_linear(b,'time')
+        amean = a.mean('M')
+        sigobs = b.std('time')
+        sigsig = amean.std('time')
+        if (resamp>0):
+            iterations = resamp
+            ens_size = 1
+            a_resamp = xs.resample_iterations_idx(a, iterations, 'M', dim_max=ens_size).squeeze()
+            sigtot = a_resamp.std('time').mean('iteration')
+        else:
+            sigtot = a.std('time').mean('M')
+        r = xs.pearson_r(amean,b,dim='time')
+        rpc = r/(sigsig/sigtot)
+        corr_list.append(r)
+        rpc_list.append(rpc.where(r>0))
+        rmse_list.append(xs.rmse(amean,b,dim='time')/sigobs)
+        msss_list.append(1-(xs.mse(amean,b,dim='time')/b.var('time')))
+        pval_list.append(xs.pearson_r_eff_p_value(amean,b,dim='time'))
+        sigobs_list.append(sigobs)
+        sigsig_list.append(sigsig)
+        sigtot_list.append(sigtot)
+        s2t_list.append(sigsig/sigtot)
+    corr = xr.concat(corr_list,lvalsda)
+    pval = xr.concat(pval_list,lvalsda)
+    rmse = xr.concat(rmse_list,lvalsda)
+    msss = xr.concat(msss_list,lvalsda)
+    rpc = xr.concat(rpc_list,lvalsda)
+    sigo = xr.concat(sigobs_list,lvalsda)
+    sigs = xr.concat(sigsig_list,lvalsda)
+    sigt = xr.concat(sigtot_list,lvalsda)
+    s2t  = xr.concat(s2t_list,lvalsda)
+    return xr.Dataset({'corr':corr,'pval':pval,'rmse':rmse,'msss':msss,'rpc':rpc,'sig_obs':sigo,'sig_sig':sigs,'sig_tot':sigt,'s2t':s2t})
+
+def compute_skill_seasonal(mod_da,mod_time,obs_da,climy0,climy1,nleadavg=1,nleads=1,resamp=0,detrend=False,monthly=False):
+    """
+    Computes a suite of skill scores for annual data. Includes option to use xskillscore resampling to
+    compute the mean variance of individual member time series ("sigma_total").
+    Assumes mod_time and obs_da.time are cftime arrays with year/month values.
+    """
+    corr_list = []; pval_list = []; rmse_list = []; msss_list = []; rpc_list = []
+    sigobs_list = []; sigsig_list = []; sigtot_list = []; s2t_list = []
+    
+    # convert L to leadtime values:
+    if (monthly):
+        lvals = np.arange(nleadavg)*12
+    else:
+        lvals = np.arange(nleadavg)*4
+    lvalsda = xr.DataArray(mod_da.isel(L=slice(0,nleads)).L,dims="L",name="L")
+    for i in range(nleads):
+        leadisel = lvals + i 
+        ens_ts = mod_da.isel(L=leadisel).mean('L').rename({'Y':'time'})
+        ens_time_year = mod_time.isel(L=leadisel).mean('L').dt.year
+        ens_time_month = mod_time.isel(L=leadisel).mean('L').dt.month.data[0]
+        ens_ts = ens_ts.assign_coords(time=("time",ens_time_year.data))
+        obsisel = obs_da.time.dt.month==ens_time_month
+        obs_seas = obs_da.isel(time=obsisel)
+        obs_seas = obs_seas - obs_seas.sel(time=slice(climy0,climy1)).mean('time')
+        obs_seas = obs_seas.assign_coords(time=("time",obs_seas.time.dt.year.data))
+        if (nleadavg>1):
+            obs_seas = obs_seas.rolling(time=nleadavg,min_periods=nleadavg, center=True).mean().dropna('time',how='all')
+        a,b = xr.align(ens_ts,obs_seas)
+        if detrend:
+                a = detrend_linear(a,'time')
+                b = detrend_linear(b,'time')
+        amean = a.mean('M')
+        sigobs = b.std('time')
+        sigsig = amean.std('time')
+        if (resamp>0):
+            iterations = resamp
+            ens_size = 1
+            a_resamp = xs.resample_iterations_idx(a, iterations, 'M', dim_max=ens_size).squeeze()
+            sigtot = a_resamp.std('time').mean('iteration')
+        else:
+            sigtot = a.std('time').mean('M')
+        r = xs.pearson_r(amean,b,dim='time')
+        rpc = r/(sigsig/sigtot)
+        corr_list.append(r)
+        rpc_list.append(rpc.where(r>0))
+        rmse_list.append(xs.rmse(amean,b,dim='time')/sigobs)
+        msss_list.append(1-(xs.mse(amean,b,dim='time')/b.var('time')))
+        pval_list.append(xs.pearson_r_eff_p_value(amean,b,dim='time'))
+        sigobs_list.append(sigobs)
+        sigsig_list.append(sigsig)
+        sigtot_list.append(sigtot)
+        s2t_list.append(sigsig/sigtot)
+    corr = xr.concat(corr_list,lvalsda)
+    pval = xr.concat(pval_list,lvalsda)
+    rmse = xr.concat(rmse_list,lvalsda)
+    msss = xr.concat(msss_list,lvalsda)
+    rpc = xr.concat(rpc_list,lvalsda)
+    sigo = xr.concat(sigobs_list,lvalsda)
+    sigs = xr.concat(sigsig_list,lvalsda)
+    sigt = xr.concat(sigtot_list,lvalsda)
+    s2t  = xr.concat(s2t_list,lvalsda)
+    return xr.Dataset({'corr':corr,'pval':pval,'rmse':rmse,'msss':msss,'rpc':rpc,'sig_obs':sigo,'sig_sig':sigs,'sig_tot':sigt,'s2t':s2t})
+
+
+def compute_resampskill_annual(mod_da,mod_time,obs_da,nyear=1,nleads=1,detrend=False,resamp=0,mean=True):
+    """
+    Computes a suite of skill scores for annual data.
+    Assumes mod_time and obs_da.time both contain year values.
+    """
+    dslist = []
+    if (nyear>1):
+        obs_ts = obs_da.rolling(time=nyear,min_periods=nyear, center=True).mean().dropna('time')
+    lvals = np.arange(nyear)
+    lvalsda = xr.DataArray(np.arange(nleads),dims="L",name="L")
+    for l in mod_da.iteration.values:
+        corr_list = []; pval_list = []; rmse_list = []; msss_list = []; rpc_list = []
+        sigobs_list = []; sigsig_list = []; sigtot_list = []; s2t_list = []
+        for i in range(nleads):
+            ens_ts = mod_da.sel(iteration=l).isel(L=lvals+i).mean('L').rename({'Y':'time'})
+            ens_time_year = mod_time.isel(L=lvals+i).mean('L').data
+            ens_ts = ens_ts.assign_coords(time=("time",ens_time_year))
+            a,b = xr.align(ens_ts,obs_ts)
+            b = b - b.mean('time')
+            if detrend:
+                a = detrend_linear(a,'time')
+                b = detrend_linear(b,'time')
+            amean = a.mean('M')
+            sigobs = b.std('time')
+            sigsig = amean.std('time')
+            if (resamp>0):
+                iterations = resamp
+                ens_size = 1
+                a_resamp = xs.resample_iterations_idx(a, iterations, 'M', dim_max=ens_size).squeeze()
+                sigtot = a_resamp.std('time').mean('iteration')
+            else:
+                sigtot = a.std('time').mean('M')
+            r = xs.pearson_r(amean,b,dim='time')
+            rpc = r/(sigsig/sigtot)
+            corr_list.append(r)
+            rpc_list.append(rpc.where(r>0))
+            rmse_list.append(xs.rmse(amean,b,dim='time')/sigobs)
+            msss_list.append(1-(xs.mse(amean,b,dim='time')/b.var('time')))
+            pval_list.append(xs.pearson_r_eff_p_value(amean,b,dim='time'))
+            sigsig_list.append(sigsig)
+            sigtot_list.append(sigtot)
+            s2t_list.append(sigsig/sigtot)
+        corr = xr.concat(corr_list,lvalsda)
+        pval = xr.concat(pval_list,lvalsda)
+        rmse = xr.concat(rmse_list,lvalsda)
+        msss = xr.concat(msss_list,lvalsda)
+        rpc = xr.concat(rpc_list,lvalsda)
+        sigs = xr.concat(sigsig_list,lvalsda)
+        sigt = xr.concat(sigtot_list,lvalsda)
+        s2t  = xr.concat(s2t_list,lvalsda)
+        dslist.append(xr.Dataset({'corr':corr,'pval':pval,'rmse':rmse,'msss':msss,'rpc':rpc,'sig_sig':sigs,'sig_tot':sigt,'s2t':s2t}))
+    dsout = xr.concat(dslist,dim='iteration')
+    if (mean):
+        dsout = dsout.mean('iteration')
+    return dsout
+
+def compute_resampskill_seasonal(mod_da,mod_time,obs_da,climy0,climy1,nleadavg=1,nleads=1,detrend=False,resamp=0,mean=True):
+    """
+    Computes a suite of skill scores for annual data. Includes option to use xskillscore resampling to
+    compute the mean variance of individual member time series ("sigma_total").
+    Assumes mod_time and obs_da.time both contain year values.
+    """
+    dslist = []
+    lvals = np.arange(nleadavg)*4
+    # Convert to leadtime values
+    lvalsda = xr.DataArray(mod_da.isel(L=slice(0,nleads)).L-2,dims="L",name="L")
+    
+    for l in mod_da.iteration.values:
+        corr_list = []; pval_list = []; rmse_list = []; msss_list = []; rpc_list = []
+        sigobs_list = []; sigsig_list = []; sigtot_list = []; s2t_list = []
+        for i in range(nleads):
+            leadisel = lvals + i 
+            ens_ts = mod_da.sel(iteration=l).isel(L=leadisel).mean('L').rename({'Y':'time'})
+            ens_time_year = mod_time.isel(L=leadisel).mean('L').dt.year
+            ens_time_month = mod_time.isel(L=leadisel).mean('L').dt.month.data[0]
+            ens_ts = ens_ts.assign_coords(time=("time",ens_time_year.data))
+            obsisel = obs_da.time.dt.month==ens_time_month
+            obs_seas = obs_da.isel(time=obsisel)
+            obs_seas = obs_seas - obs_seas.sel(time=slice(climy0,climy1)).mean('time')
+            obs_seas = obs_seas.assign_coords(time=("time",obs_seas.time.dt.year.data))
+            if (nleadavg>1):
+                obs_seas = obs_seas.rolling(time=nleadavg,min_periods=nleadavg, center=True).mean().dropna('time',how='all')
+            a,b = xr.align(ens_ts,obs_seas)
+            if detrend:
+                a = detrend_linear(a,'time')
+                b = detrend_linear(b,'time')
+            amean = a.mean('M')
+            sigobs = b.std('time')
+            sigsig = amean.std('time')
+            if (resamp>0):
+                iterations = resamp
+                ens_size = 1
+                a_resamp = xs.resample_iterations_idx(a, iterations, 'M', dim_max=ens_size).squeeze()
+                sigtot = a_resamp.std('time').mean('iteration')
+            else:
+                sigtot = a.std('time').mean('M')
+            r = xs.pearson_r(amean,b,dim='time')
+            rpc = r/(sigsig/sigtot)
+            corr_list.append(r)
+            rpc_list.append(rpc.where(r>0))
+            rmse_list.append(xs.rmse(amean,b,dim='time')/sigobs)
+            msss_list.append(1-(xs.mse(amean,b,dim='time')/b.var('time')))
+            pval_list.append(xs.pearson_r_eff_p_value(amean,b,dim='time'))
+            sigobs_list.append(sigobs)
+            sigsig_list.append(sigsig)
+            sigtot_list.append(sigtot)
+            s2t_list.append(sigsig/sigtot)
+        corr = xr.concat(corr_list,lvalsda)
+        pval = xr.concat(pval_list,lvalsda)
+        rmse = xr.concat(rmse_list,lvalsda)
+        msss = xr.concat(msss_list,lvalsda)
+        rpc = xr.concat(rpc_list,lvalsda)
+        sigo = xr.concat(sigobs_list,lvalsda)
+        sigs = xr.concat(sigsig_list,lvalsda)
+        sigt = xr.concat(sigtot_list,lvalsda)
+        s2t  = xr.concat(s2t_list,lvalsda)
+        dslist.append(xr.Dataset({'corr':corr,'pval':pval,'rmse':rmse,'msss':msss,'rpc':rpc,'sig_sig':sigs,'sig_tot':sigt,'s2t':s2t}))
+    dsout = xr.concat(dslist,dim='iteration')
+    if (mean):
+        dsout = dsout.mean('iteration')
+    return dsout
+
+
